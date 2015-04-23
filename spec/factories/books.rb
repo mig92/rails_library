e335835d02e67a32217fccdaeabd5ee8e0632485
Alejandro Ferro <alejandroferro.1988@gmail.com>
2015-04-23 20:04:12 +0200

    added book scaffold

---
+++ spec/factories/books.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :book do
+    title "MyString"
+    author "MyString"
+    isbn "MyString"
+    format "MyString"
+  end
+end
