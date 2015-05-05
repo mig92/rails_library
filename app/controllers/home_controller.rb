e2f818be386078ace347ccaac60a8ec87b539353
Alejandro Ferro <alejandroferro.1988@gmail.com>
2015-05-05 20:05:18 +0200

    Added intro views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
+
   def index
     @users = User.all
     @books = Book.all
+    if user_signed_in?
+      @loans = Loan.find_all_by_user_id(current_user.id)
+    end
   end
 end
